4fd040f61f9ced9a6b515a4acd263aed9c33a86c
denisraleina <devsoftware907@gmail.com>
2021-02-01 12:03:31 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Khula:wght@300;400;700;800&display=swap" rel="stylesheet"></link>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@700&display=swap" rel="stylesheet"></link>
+                    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet"></link>
                 </Head>
                 <body>
                     <Main />
